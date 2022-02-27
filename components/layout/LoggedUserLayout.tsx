d2af91b5ea3e4519d58937199dbee00b0193d3c8
Flemis <coderflemis@gmail.com>
2022-02-27 10:35:49 -0300

    test

--- components/layout/LoggedUserLayout.tsx
+++ components/layout/LoggedUserLayout.tsx
@@ -4,7 +4,6 @@ function LoggedUserLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex min-h-screen flex-col">
       <Header />
-      asdasdsad
       {children}
     </div>
   )
